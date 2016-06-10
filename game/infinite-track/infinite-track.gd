82f37eb6b92fc69621f16339b1bc18f2f5a2722e
Alejandro Seguí <alesegdia@gmail.com>
2016-06-10 03:31:22 +0200

    adding some colour to our life

--- game/infinite-track/infinite-track.gd
+++ game/infinite-track/infinite-track.gd
@@ -5,12 +5,17 @@ export var height = 3
 export var width = 10
 export var quad_extent = 0.5
 
+var bg_color = Color( 0.9, 0.141, 0.208 )
+var line_color = Color( 0.675, 0.169, 0.392 )
+
 func _ready():
 	set_process(true)
 	pass
 
 func _process(delta):
 	begin(VisualServer.PRIMITIVE_LINES, get_material_override())
+	set_uv(Vector2(0, 0))
+	set_color(line_color)
 	for x in range(width):
 		for y in range(height):
 			var xx = x * quad_extent
