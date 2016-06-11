4383548be24b91d7e94990ddf03e2a54281aeb5a
Alejandro Seguí <alesegdia@gmail.com>
2016-06-11 16:30:06 +0200

    working on track movement

--- game/infinite-track/InfiniteTrack.gd
+++ game/infinite-track/InfiniteTrack.gd
@@ -8,20 +8,23 @@ export var quad_extent = 0.5
 export var line_color = Color( 0.675, 0.169, 0.392 )
 
 var ig
+var timer = 0
+var y_offset = 0
 
 func _ready():
 	ig = get_node("ImmediateGeometry")
 	set_process(true)
 	pass
 
-func renderTrack():
+func renderTrack( delta ):
+	ig.clear()
 	ig.begin(VisualServer.PRIMITIVE_LINES, ig.get_material_override())
 	ig.set_uv(Vector2(0, 0))
 	ig.set_color(line_color)
 	for x in range(width):
 		for y in range(height):
 			var xx = x * quad_extent
-			var yy = y * quad_extent
+			var yy = y * quad_extent + timer
 			var v00 = Vector3( xx - width * quad_extent/2, 0, yy )
 			var v10 = Vector3( xx + quad_extent - width * quad_extent /2, 0, yy )
 			var v01 = Vector3( xx - width * quad_extent /2, 0, yy + quad_extent )
@@ -40,6 +43,9 @@ func renderTrack():
 	ig.end()
 	pass
 
-func _process(delta):
-	renderTrack()
+func _process( delta ):
+	timer += delta
+	if timer > quad_extent:
+		timer = 0
+	renderTrack( delta )
 	pass
