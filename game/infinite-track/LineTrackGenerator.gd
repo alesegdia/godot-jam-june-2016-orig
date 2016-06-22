4cdd87f607de660f5229f0e7d57c9f59d644d08b
Alejandro Seguí <alesegdia@gmail.com>
2016-06-22 22:37:55 +0200

    working on line track generator; still not ready

--- game/infinite-track/LineTrackGenerator.gd
+++ game/infinite-track/LineTrackGenerator.gd
@@ -3,20 +3,23 @@ extends "TrackGenerator.gd"
 var slope
 var line_start
 
-func _init(plinestart, pslope, args).(args):
+func _init(plinestart, pslope, pzoffset, args).(args):
 	slope = pslope
 	line_start = plinestart
-	pass
+	z_current = pzoffset
 
-func f(y):
-	return y * slope
+func f(z):
+	return z * slope
 
 func plot(x, val):
 	next_row[x] = val
 
-func sample(x0, x1, inc):
-	for x in range(x0, x1, inc):
-		plot(f(x), 1)
+func sample(z0, y1, inc):
+	for z in range(z0, z1, inc):
+		plot(f(z), 1)
+
+func ssample():
+	pass
 
 func gen_next_row():
 	next_row = Array()
